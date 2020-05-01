d8b63cacb780a2dffb8d4091fe38e606353e790e
Rachel Schieferstein <violenza@gmail.com>
2020-05-01 12:03:54 -0700

    action dispatch tests working in root reducer

--- src/components/__tests__/reducers/index-reducer.test.js
+++ src/components/__tests__/reducers/index-reducer.test.js
@@ -22,4 +22,26 @@ describe("rootReducer", () => {
     expect(store.getState().formVisible).toEqual(formVisibleReducer(undefined, { type: null }));
   });
 
+  test('Check that initial state of ticketListReducer matches root reducer', () => {
+    const action = {
+      type: 'ADD_KEG',
+      name: "sadad",
+      brand: "sdddd",
+      alcoholContent: 33,
+      price: 22,
+      pints: 9,
+      id: "3"
+    }
+    store.dispatch(action);
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, action));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisible).toEqual(formVisibleReducer(undefined, action));
+  });
+
 });
