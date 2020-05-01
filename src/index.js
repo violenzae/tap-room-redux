25f7f08dcb81427eca213c6279181b88f4e35548
Rachel Schieferstein <violenza@gmail.com>
2020-05-01 10:05:48 -0700

    creating store and adding devtools redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import reducer from './reducers/keg-list-reducer';
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <React.StrictMode>
