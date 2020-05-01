f9b0c80c404923f96b382f5354533b0fd59e47de
Rachel Schieferstein <violenza@gmail.com>
2020-05-01 09:54:03 -0700

    adding test for list/add reducer to see if format correct

--- src/components/__tests__/reducers/keg-list-reducer.test.js
+++ src/components/__tests__/reducers/keg-list-reducer.test.js
@@ -17,4 +17,28 @@ describe('kegListReducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
 
+  test('Should successfully add new keg data to masterKegList', () => {
+    const { name, brand, alcoholContent, price, pints, id} = sampleKeg;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      alcoholContent: alcoholContent,
+      price: price,
+      pints: pints,
+      id: id
+    };
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        alcoholContent: alcoholContent,
+        price: price,
+        pints: pints,
+        id: id
+      }
+    });
+  });
+
+
 })
--- src/components/reducers/keg-list-reducer.js
+++ src/components/reducers/keg-list-reducer.js
@@ -1,7 +1,7 @@
 export default (state = {}, action) => {
   const { name, brand, alcoholContent, price, pints, id } = action;
   switch (action.type) {
-  case 'ADD_KREG':
+  case 'ADD_KEG':
     return Object.assign({}, state, {
       [id]: {
         name: name,
