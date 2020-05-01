8b2bf0144761abfd68bbc677bffdbd964fb4649b
Rachel Schieferstein <violenza@gmail.com>
2020-05-01 11:40:06 -0700

    adding test for toggle reducer

---
+++ src/components/__tests__/reducers/form-visible-reducer.test.js
@@ -0,0 +1,8 @@
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+
+describe("formVisibleReducer", () => {
+
+  test('Should toggle form visibility state to true', () => {
+    expect(formVisibleReducer(false, { type: 'TOGGLE_FORM' })).toEqual(true);
+  });
+});
